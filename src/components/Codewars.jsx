6dbbf491622b1c8fe537a35ff9865bdca0804c39
AnilBASARAN <nlbsrn@gmail.com>
2024-12-03 07:39:33 +0300

    first commit

---
+++ src/components/Codewars.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { data } from "../data/data.js";
+
+
+const Work = () => {
+
+    // projects file
+    const project = data;
+    //setProject(data);
+  
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-red-00  bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col m-12  justify-center w-full h-full'>
+        <div className='pb-8 m-12'>
+          <p className='text-4xl font-bold inline border-b-4  text-gray-300 border-pink-600'>
+            Codewars
+          </p>
+          <div className='flex flex-col md:justify-between md:flex-row'>
+          <p className='py-6'> These are some of the problems and solutions that I came up with at Codewars </p>
+          
+
+          </div>
+        </div>
+<div className='projects-all grid sm:grid-cols-2 gap-12 ' >
+<div className="grid sm:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4">
+          
+          {/* Gird Item */}
+          {project.map((item, index) => (
+  <div
+    key={index}
+    style={{ backgroundImage: `url(${item.image})` }}
+    className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+  >
+    {/* Hover effect for images */}
+    <div className="opacity-0 group-hover:opacity-100 ">
+      <span className="text-2xl font bold text-white tracking-wider ">
+        {item.name}
+      </span>
+      <div className="pt-8 text-center ">
+        {/* eslint-disable-next-line */}
+        <a href={item.github} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Code
+          </button>
+        </a>
+        {/* eslint-disable-next-line */}
+        <a href={item.live} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Live
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+))}
+
+
+</div>
+{/**/}
+<div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+          
+          {/* Gird Item */}
+          {project.map((item, index) => (
+  <div
+    key={index}
+    style={{ backgroundImage: `url(${item.image})` }}
+    className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+  >
+    {/* Hover effect for images */}
+    <div className="opacity-0 group-hover:opacity-100 ">
+      <span className="text-2xl font bold text-white tracking-wider ">
+        {item.name}
+      </span>
+      <div className="pt-8 text-center ">
+        {/* eslint-disable-next-line */}
+        <a href={item.github} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Code
+          </button>
+        </a>
+        {/* eslint-disable-next-line */}
+        <a href={item.live} target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Live
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+))}
+
+
+</div>
+
+</div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Work;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import Sql from '../assets/sql.png';
+import JavaScript from '../assets/javascript.png';
+import Java from '../assets/java.png';
+import ReactImg from '../assets/react.png';
+import Spring from '../assets/spring.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full flex items-center justify-center h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px]   flex  flex-col justify-center w-full h-full'>
+          <div className='mb-12'>
+              <p className='text-4xl font-bold  inline border-b-4 border-pink-600 '>Stacks that I worked with</p>
+              
+          </div>
+          <div className='flex justify-between'>
+          <p className='py-4'>Mern Stack</p>
+          <p className='py-4'>Java Full Stack</p>
+          </div>
+
+          <div className='stacks-container flex gap-12' >
+          <div className='first-stack w-full grid  grid-cols-2 sm:grid-cols-4 gap-4  text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110  duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                  <p className='my-4'>REACT</p>
+              </div>
+            
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
+                  <p className='my-4'>NODE JS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
+                  <p className='my-4'>MONGO DB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
+                  <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <p className='my-4'>FIREBASE</p>
+              </div>
+          </div>
+          {/**/}
+
+          <div className='first-stack w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+             
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Java} alt="HTML icon" />
+                  <p className='my-4'>JAVA</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Spring} alt="HTML icon" />
+                  <p className='my-4'>SPRING</p>
+              </div>
+             
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+                  <p className='my-4'>GitHub</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Sql} alt="HTML icon" />
+                  <p className='my-4'>SQL DB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
+                  <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <p className='my-4'>FIREBASE</p>
+              </div>
+          </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
