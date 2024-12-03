8a31a31b126e6f567f3bcf92683c93886940601c
AnilBASARAN <nlbsrn@gmail.com>
2024-12-03 19:37:09 +0300

    contact redacted

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Space from "./components/Space";
 
 
