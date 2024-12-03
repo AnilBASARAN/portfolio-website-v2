54e23a90454e765c886cdd2de9da7ad5f97d30c8
AnilBASARAN <nlbsrn@gmail.com>
2024-12-03 09:48:51 +0300

    kyu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,10 +19,11 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
+          I started with Java and Spring... Ended up learning Node.js and all thats come with it.I’m a full-stack developer specializing in building (and occasionally
+          designing) Currently, I am learning and facinated by authorization, tokens and all that good stuff, Enjoying my newly aquired skills MongoDb,Node, still far away being an expert but hey, thats the journey... I’m focused on
           building responsive full-stack web applications.
         </p>
+        <p>Also obsessed with one liner js codewar solutions and occasionally trying to solve a 4kyu...</p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
             View Work
