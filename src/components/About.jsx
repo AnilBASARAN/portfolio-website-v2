264ee08dc7e0c2ee4142d517153d2504f52fa49f
AnilBASARAN <nlbsrn@gmail.com>
2024-12-04 05:25:51 +0300

    colorfull mind

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,10 +18,10 @@ const About = () => {
             </div>
             <div className='flex flex-col gap-2 items-center  justify-center sm:text-center'>
               <p className='text-xl'>Graduated from Yildiz Technical University.</p>
-              <p className='text-green-400'>As a Survey Engineer</p>
+              <p className='text-gray-400'>As a Survey <span className=' text-yellow-200 font-semibold text-xl'>Engineer</span></p>
               <p>Worked on several diciplines including building <span className='text-sm text-pink-600'>bullet train paths</span>, highways,<span className='text-xl text-red-400'>and structures  related</span></p> 
               <p>Also worked in constructring. Building <span className=' text-cyan-600 font-semibold text-xl'>Skyscrapers</span>...Found out Survey engineering not that glamarous.</p>
-              <p> Ended up being a Co-Founder of a DropShipping company and supervise and run it for 7 years.</p>
+              <p> Ended up being a <span className=' text-yellow-200 font-semibold text-xl'>Co Founder</span> of a DropShipping company and supervise and run it for 7 years.</p>
                 <p className='text-green-400'>I tried my first <span className=' text-cyan-600 font-semibold text-3xl'>"Hello World"</span> and ended up loving it </p>
                 <p>Learn or tried to learn how to code in order to automate it,After harsh regulatory changes we ended up closing shop and here we are, </p>  
               <p className='text-green-700'>I am finally found the time and the energy and motivation to do it full time.</p>
