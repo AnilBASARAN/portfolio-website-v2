a5c59144fcf77a4828aded38375bf5b40a4adfb0
AnilBASARAN <nlbsrn@gmail.com>
2024-12-08 22:10:23 +0300

    changed the work to projects to be more accurate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         </li>
         <li>
           <Link to='work' smooth={true} duration={500}>
-            Work
+            Projects
           </Link>
         </li>
 
@@ -78,7 +78,7 @@ const Navbar = () => {
   </li>
   <li className='py-6 text-4xl text-center'>
     <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-      Work
+      Projects
     </Link>
   </li>
   <li className='py-6 text-4xl text-center'>
