edc9eeea6549657f48d5e85958a5a4ca5568f1b8
AnilBASARAN <nlbsrn@gmail.com>
2024-12-08 22:07:47 +0300

    changed the hamburger menu, added the c.v.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,80 +50,72 @@ const Navbar = () => {
         
       </ul>
 
-      {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
+{/* Hamburger */}
+<div onClick={handleClick} className='md:hidden z-10 absolute top-6 right-6'>
+  {!nav ? <FaBars /> : <FaTimes />}
+</div>
 
-      {/* Mobile menu */}
-      <ul
-        className={
-          !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
-        }
-      >
-        <li className='py-6 text-4xl'>
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+{/* Mobile menu */}
+<ul
+  className={`${
+    nav ? 'translate-x-0' : '-translate-x-full'
+  } absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center transition-transform duration-500`}
+>
+  <li className='py-6 text-4xl text-center'>
+    <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+      Home
+    </Link>
+  </li>
+  <li className='py-6 text-4xl text-center'>
+    <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+      About
+    </Link>
+  </li>
+  <li className='py-6 text-4xl text-center'>
+    <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+      Skills
+    </Link>
+  </li>
+  <li className='py-6 text-4xl text-center'>
+    <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+      Work
+    </Link>
+  </li>
+  <li className='py-6 text-4xl text-center'>
+  <a
+    onClick={handleClick}
+    href='https://drive.google.com/file/d/1Y_ANXWkttacQ__HgIJ2TdmfkvB0V75s3/view'
+    target='_blank'  // Opens the link in a new tab
+    rel='noopener noreferrer'  // Adds security when opening a new tab
+  >
+    C.V.
+  </a>
+</li>
 
-      {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/anılbaşaran/'
-            >
-              Linkedin <FaLinkedin size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/AnilBASARAN'
-            >
-              Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://x.com/nlbsrn'
-            >
-              Twitter <FaTwitter size={30} />
-            </a>
-          </li>
-          
-        </ul>
-      </div>
+</ul>
+
+{/* Social icons */}
+<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+  <ul>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+      <a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/anılbaşaran/'>
+        Linkedin <FaLinkedin size={30} />
+      </a>
+    </li>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+      <a className='flex justify-between items-center w-full text-gray-300' href='https://github.com/AnilBASARAN'>
+        Github <FaGithub size={30} />
+      </a>
+    </li>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+      <a className='flex justify-between items-center w-full text-gray-300' href='https://x.com/nlbsrn'>
+        Twitter <FaTwitter size={30} />
+      </a>
+    </li>
+  </ul>
+</div>
+
+     
     </div>
   );
 };
