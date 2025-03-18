dabfcf26bbddfc7adda147ae9614231fe29b4abb
AnilBASARAN <nlbsrn@gmail.com>
2025-03-18 19:00:43 +0300

    adjusted the padding between components

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Github = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full bg-[#0a192f]'>
     {/* Container */}
     <div className='max-w-[1000px] mx-auto flex flex-col justify-center'>
         <img src="https://raw.githubusercontent.com/AnilBASARAN/AnilBASARAN/output/dist/snake.svg" alt="Snake" />
