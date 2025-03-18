17f36686ece308692df1ce30eff40f82872e3a08
AnilBASARAN <nlbsrn@gmail.com>
2025-03-18 17:53:05 +0300

    made the necesarry changes, changed the projects and its links

--- src/data/projectJava.data.js
+++ src/data/projectJava.data.js
@@ -1,37 +1,37 @@
 
-import Macera from '../assets/projects/maceraJava.png'
-import Mayın from '../assets/projects/mayınJava.png'
-import Turism from '../assets/projects/turizmacenta.png'
-import Veteriner from '../assets/projects/veteriner.png'
+import Starwars from '../assets/projects/starwars.png'
+import SportsCenter from '../assets/projects/sportCenter.png'
+import RandomPerson from '../assets/projects/randomPerson.png'
+import vetManagement from '../assets/projects/vetManagement.png'
 
 export const projectJavaData=[
     {
         id:1,
-        name:"Java Macera Oyunu",
-        image:Macera,
-        github:"https://github.com/AnilBASARAN/patika-hafta4-macera-oyunu",
-        live:"",
+        name:"Starwars",
+        image:Starwars,
+        github:"https://github.com/AnilBASARAN/patika-react-starwars",
+        live:"https://starwars-anilbasaran.netlify.app",
     },
     {
         id:2,
-        name:"Java Mayın Oyunu",
-        image:Mayın,
-        github:"https://github.com/AnilBASARAN/patika-hafta3-mayin-tarlasi",
-        live:"",
+        name:"SportsCenter",
+        image:SportsCenter,
+        github:"https://github.com/AnilBASARAN/patika-sport-center",
+        live:"https://anilbasaran-sportcenter.netlify.app",
     },
     {
         id:3,
-        name:"Java Sale Page",
-        image:Turism,
-        github:"https://github.com/AnilBASARAN/patika-hafta8-turizm-bitirme-odevi",
-        live:"",
+        name:"RandomPerson",
+        image:RandomPerson,
+        github:"https://github.com/AnilBASARAN/randomPersonV2",
+        live:"https://randompersonfetcher.netlify.app",
     },
     {
         id:4,
-        name:"Veteriner Java Uygulaması",
-        image:Veteriner,
-        github:"https://github.com/AnilBASARAN/patika-hafta13-veteriner-projesi",
-        live:"",
+        name:"Vet Management",
+        image:vetManagement,
+        github:"https://github.com/AnilBASARAN/patika-vet-app",
+        live:"https://anil-basaran-patika-vet-app.netlify.app",
     },
 
 
